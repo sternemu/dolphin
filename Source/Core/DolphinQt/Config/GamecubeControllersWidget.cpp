ae4ee1a4a24173b8263da4ecf3a9f53689c114f4
izzy2lost <izzynochill@gmail.com>
2024-06-13 14:45:22 -0400

    Add triforce support

--- Source/Core/DolphinQt/Config/GamecubeControllersWidget.cpp
+++ Source/Core/DolphinQt/Config/GamecubeControllersWidget.cpp
@@ -41,6 +41,7 @@ static constexpr std::array s_gc_types = {
 #ifdef HAS_LIBMGBA
     SIDeviceName{SerialInterface::SIDEVICE_GC_GBA_EMULATED, _trans("GBA (Integrated)")},
 #endif
+    SIDeviceName{SerialInterface::SIDEVICE_AM_BASEBOARD, _trans("AM_BASEBOARD")},
     SIDeviceName{SerialInterface::SIDEVICE_GC_GBA, _trans("GBA (TCP)")},
     SIDeviceName{SerialInterface::SIDEVICE_GC_KEYBOARD, _trans("Keyboard Controller")},
 };
