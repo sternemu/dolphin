22d81a8c3d1c2fcbe0098ab11e77e93c4f4cfc9f
izzy2lost <izzynochill@gmail.com>
2024-06-13 13:12:35 -0400

    Add triforce support

--- Source/Core/Common/CommonTypes.h
+++ Source/Core/Common/CommonTypes.h
@@ -18,6 +18,16 @@
 #define LONG int
 #endif
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
