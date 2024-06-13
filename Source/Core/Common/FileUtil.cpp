6131cf4b3c55e34a1f7bb852e5b6b0cbb2bcb3c5
izzy2lost <izzynochill@gmail.com>
2024-06-13 15:01:18 -0400

    Triforce user path

--- Source/Core/Common/FileUtil.cpp
+++ Source/Core/Common/FileUtil.cpp
@@ -952,7 +952,9 @@ static void RebuildUserDirectories(unsigned int dir_index)
     s_user_paths[D_GBAUSER_IDX] = s_user_paths[D_USER_IDX] + GBA_USER_DIR DIR_SEP;
     s_user_paths[D_GBASAVES_IDX] = s_user_paths[D_GBAUSER_IDX] + GBASAVES_DIR DIR_SEP;
     s_user_paths[F_GBABIOS_IDX] = s_user_paths[D_GBAUSER_IDX] + GBA_BIOS;
-
+    
+    s_user_paths[D_TRIUSER_IDX] = s_user_paths[D_USER_IDX] + TRIFORCE_USER_DIR DIR_SEP;
+    
     // The shader cache has moved to the cache directory, so remove the old one.
     // TODO: remove that someday.
     File::DeleteDirRecursively(s_user_paths[D_USER_IDX] + SHADERCACHE_LEGACY_DIR DIR_SEP);
