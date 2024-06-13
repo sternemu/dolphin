b1d995a175aadff07766bb969daae872c05a7585
izzy2lost <izzynochill@gmail.com>
2024-06-13 14:05:29 -0400

    Add triforce support

--- Source/Core/Core/HW/SI/SI.h
+++ Source/Core/Core/HW/SI/SI.h
@@ -56,6 +56,7 @@ public:
   void RemoveEvent(int device_number);
 
   void UpdateDevices();
+  u32 GetInLength(void);
 
   void RemoveDevice(int device_number);
   void AddDevice(SIDevices device, int device_number);
