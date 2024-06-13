e83c06bd38b28ae41e028adcf25adef666dcada1
izzy2lost <izzynochill@gmail.com>
2024-06-13 14:03:56 -0400

    Add triforce support

--- Source/Core/Core/HW/SI/SI.cpp
+++ Source/Core/Core/HW/SI/SI.cpp
@@ -130,6 +130,14 @@ constexpr s32 ConvertSILengthField(u32 field)
   return ((field - 1) & SI_XFER_LENGTH_MASK) + 1;
 }
 
+u32 GetInLength(void)
+{
+  auto& system = Core::System::GetInstance();
+  auto& state = system.GetSerialInterfaceState().GetData();
+  const s32 expected_response_length = ConvertSILengthField(state.com_csr.INLNGTH);
+  return expected_response_length;
+}
+
 void SerialInterfaceManager::GlobalRunSIBuffer(Core::System& system, u64 user_data, s64 cycles_late)
 {
   system.GetSerialInterface().RunSIBuffer(user_data, cycles_late);
